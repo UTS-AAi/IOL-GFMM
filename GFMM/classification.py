85cae70a1d534183a0edd0e5631ea1c120403888
Thanh Tung Khuat <thanhtung09t2@gmail.com>
2019-11-02 23:49:13 +1100

    update code

--- GFMM/classification.py
+++ GFMM/classification.py
@@ -12,7 +12,7 @@ import numpy as np
 from functionhelper.measurehelper import manhattan_distance
 from functionhelper.membershipcalc import memberG
 from functionhelper.bunchdatatype import Bunch
-from functionhelper import device, GPU_Computing_Threshold, is_Have_GPU, UNLABELED_CLASS
+from functionhelper import UNLABELED_CLASS
 
 def predict(V, W, classId, XlT, XuT, patClassIdTest, gama = 1, oper = 'min'):
     """
